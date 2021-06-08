054544f517eb6d9527e798fff0b0eeb7ba33edab
Satrajit Ghosh <satra@mit.edu>
2021-06-08 16:25:09 -0400

    fix: remove readOnly from constants

--- dandischema/models.py
+++ dandischema/models.py
@@ -186,6 +186,7 @@ class DandiBaseModel(BaseModel, metaclass=DandiBaseModelMetaclass):
                         del value["enum"]
                     else:
                         value["const"] = value["default"]
+                    del value["readOnly"]
 
 
 class PropertyValue(DandiBaseModel):
