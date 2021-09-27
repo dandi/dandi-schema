9a75d157a20b201c757c95e68bdd253b386fdb77
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-09-27 12:37:06 -0400

    Split ValidationError into jsonschema and pydantic
    There are now two different exceptions, `JsonschemaValidationError` and
    `PydanticValidationError`, to better communicate the difference between
    the two validation modes.
    
    This makes it much easier to consume errors, since the two validators
    produce errors in vastly different shapes.

--- dandischema/exceptions.py
+++ dandischema/exceptions.py
@@ -1,9 +1,18 @@
-class ValidationError(ValueError):
-    """Validation errors were detected.
+class JsonschemaValidationError(ValueError):
+    """Validation errors were detected by jsonschema.
 
     All errors are contained in the .args[0] of the exception instance.
     """
+    @property
+    def errors(self):
+        return self.args[0]
 
+
+class PydanticValidationError(ValueError):
+    """Validation errors were detected by pydantic.
+
+    All errors are contained in the .args[0] of the exception instance.
+    """
     @property
     def errors(self):
         return self.args[0]
