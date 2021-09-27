4ad67b764723c9cd43c5fca27772eb6792601173
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-09-27 12:40:30 -0400

    Throw the correct error class

--- dandischema/metadata.py
+++ dandischema/metadata.py
@@ -13,7 +13,7 @@ from .consts import (
     ALLOWED_VALIDATION_SCHEMAS,
     DANDI_SCHEMA_VERSION,
 )
-from .exceptions import ValidationError
+from .exceptions import JsonschemaValidationError, PydanticValidationError
 from . import models
 from .utils import _ensure_newline, version2tuple
 
@@ -115,7 +115,7 @@ def _validate_obj_json(data, schema, missing_ok=False):
             continue
         error_list.append([error])
     if error_list:
-        raise ValidationError(error_list)
+        raise JsonschemaValidationError(error_list)
 
 
 def _validate_dandiset_json(data: dict, schema_dir: str) -> None:
@@ -195,7 +195,7 @@ def validate(
             if not missing_ok or el["msg"] != "field required":
                 messages.append(el)
         if messages:
-            raise ValidationError(messages)
+            raise PydanticValidationError(messages)
 
 
 def migrate(
