2f19a15ab89beeb9ebbafab860e9ef1e49a9d3a1
John T. Wodder II <git@varonathe.org>
2021-12-15 08:15:30 -0500

    Stop using `setup_requires`

--- setup.py
+++ setup.py
@@ -19,11 +19,5 @@ if sys.version_info < (3,):
         "You are using %s" % sys.version
     )
 
-# Give setuptools a hint to complain if it's too old a version
-# Should match pyproject.toml
-SETUP_REQUIRES = ["setuptools >= 42.0.0", "versioningit ~= 0.1.0"]
-# This enables setuptools to install wheel on-the-fly
-SETUP_REQUIRES += ["wheel"] if "bdist_wheel" in sys.argv else []
-
 if __name__ == "__main__":
-    setup(name="dandischema", setup_requires=SETUP_REQUIRES)
+    setup(name="dandischema")
