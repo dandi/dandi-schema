a0e043c05785a526cb5bc9d94f332c85f12717a8
Isaac To <candleindark@gmail.com>
2023-12-12 20:05:27 -0800

    Correct type annotation
    Of `field.json_schema_extra` in
    `test_properties_mismatch()`

--- dandischema/tests/test_models.py
+++ dandischema/tests/test_models.py
@@ -566,11 +566,11 @@ def test_properties_mismatch() -> None:
         if not hasattr(klass, "_ldmeta") or "nskey" not in klass._ldmeta.default:
             errors.append(f"{klass} does not have nskey")
         for name, field in klass.model_fields.items():
-            nskey = (
-                "dandi"
-                if field.json_schema_extra is None
-                else field.json_schema_extra.get("nskey", "dandi")
-            )
+            if field.json_schema_extra is None:
+                nskey = "dandi"
+            else:
+                assert isinstance(field.json_schema_extra, dict)
+                nskey = cast(str, field.json_schema_extra.get("nskey", "dandi"))
             if name not in prop_names:
                 prop_names[name] = nskey
             elif nskey != prop_names[name]:
