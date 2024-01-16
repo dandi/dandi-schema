bddffd8eb7a55e39753388676c1f84a8416116ea
Yaroslav Halchenko <debian@onerussian.com>
2024-01-16 12:52:28 -0500

    Boost dandi schema version to 0.7.0 (since exported jsonschema does have incompatible changes)

--- dandischema/consts.py
+++ dandischema/consts.py
@@ -1,5 +1,5 @@
-DANDI_SCHEMA_VERSION = "0.6.4"
-ALLOWED_INPUT_SCHEMAS = ["0.4.4", "0.5.1", "0.5.2", "0.6.0", "0.6.1", "0.6.2", "0.6.3"]
+DANDI_SCHEMA_VERSION = "0.7.0"
+ALLOWED_INPUT_SCHEMAS = ["0.4.4", "0.5.1", "0.5.2", "0.6.0", "0.6.1", "0.6.2", "0.6.3", "0.6.4"]
 
 # ATM we allow only for a single target version which is current
 # migrate has a guard now for this since it cannot migrate to anything but current
