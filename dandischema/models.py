8c3a3e72490de72bee182768086585ce9899be9d
Isaac To <isaac.chun.to@gmail.com>
2025-05-23 11:03:13 -0700

    fix: add end of string anchor to `DANDI_DOI_PATTERN`
    It must be an oversight that the string anchor is missing

--- dandischema/models.py
+++ dandischema/models.py
@@ -65,7 +65,9 @@ UUID_PATTERN = (
 ASSET_UUID_PATTERN = r"^dandiasset:" + UUID_PATTERN
 VERSION_PATTERN = r"\d{6}/\d+\.\d+\.\d+"
 DANDI_DOI_PATTERN = (
-    rf"^10\.{DATACITE_DOI_ID_PATTERN}/{ID_PATTERN.lower()}\.{VERSION_PATTERN}"
+    (rf"^10\.{DATACITE_DOI_ID_PATTERN}/{ID_PATTERN.lower()}\.{VERSION_PATTERN}$")
+    if DATACITE_DOI_ID_PATTERN is not None
+    else None
 )
 DANDI_PUBID_PATTERN = rf"^{ID_PATTERN}:{VERSION_PATTERN}"
 DANDI_NSKEY = "dandi"  # Namespace for DANDI ontology
