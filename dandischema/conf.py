b58df9cda229518c7264b68f1a5dbc691579a0e1
Isaac To <isaac.chun.to@gmail.com>
2025-06-02 23:18:27 -0700

    feat: define mechanism to reset instance config
    In particular, the resetting is done through
    func arguments, not env vars.

--- dandischema/conf.py
+++ dandischema/conf.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Annotated, Optional
+from typing import Annotated, Any, Optional
 
 from pydantic import StringConstraints
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -66,3 +66,55 @@ Configuration of the DANDI instance
 This configuration holds the information used to customize the DANDI schema to a
 specific vendor
 """
+
+
+_instance_config = Config()  # Initial value is set by env vars alone
+"""
+Configuration of the DANDI instance
+
+This configuration holds the information used to customize the DANDI schema to a
+specific vendor, but it should not be accessed directly. Use `get_instance_config()`
+to obtain its value and `reset_instance_config()` to reset its value.
+"""
+
+
+def get_instance_config() -> Config:
+    """
+    Get the configuration of the DANDI instance
+
+    This configuration holds the information used to customize the DANDI schema to a
+    specific vendor.
+
+    Returns
+    -------
+    Config
+        The configuration of the DANDI instance
+    """
+    return _instance_config.model_copy(deep=True)
+
+
+def reset_instance_config(**kwargs: Any) -> None:
+    """
+    Reset the configuration of the DANDI instance returned by `get_instance_config()`
+
+    The resetting is done by re-recreating a new instance of `Config` with the keyword
+    arguments passed to this function.
+
+    Parameters
+    ----------
+    **kwargs
+        Keyword arguments to pass to the `Config` constructor
+
+    Note
+    ----
+        Use this function to override the configuration set by the environment
+        variables.
+
+        This function should be called before importing `dandischema.models` or the
+        new configuration will not have any affect in the models defined in
+        `dandischema.models`.
+
+    """
+    # todo: give a warning if the `models.py` is already imported
+    global _instance_config
+    _instance_config = Config(**kwargs)
