8359cfd12bbd8446973b80483c737e62ebbee1e5
Isaac To <isaac.chun.to@gmail.com>
2025-08-18 18:47:33 -0700

    chore: remove unused import

--- dandischema/datacite/__init__.py
+++ dandischema/datacite/__init__.py
@@ -12,7 +12,6 @@ import re
 from typing import Any, Dict, Union
 
 from jsonschema import Draft7Validator
-import requests
 
 from ..consts import NAME_PATTERN
 from ..models import LicenseType, Organization, Person, PublishedDandiset, RoleType
