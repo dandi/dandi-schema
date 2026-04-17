b688b3b76bde71eb9cfeb5078d1799c985456e29
Yaroslav Halchenko <debian@onerussian.com>
2026-04-17 16:28:07 -0400

    Import also original DANDI_NSKEY and get_schema_version into models.py
    ATM needed by genschemata helper. potentially we could just patch there.

--- dandischema/models_importstab.py
+++ dandischema/models_importstab.py
@@ -1,5 +1,9 @@
 from .models_linkml import *  # noqa: F401,F403
-from .models_orig import DANDI_INSTANCE_URL_PATTERN  # noqa: F401
+from .models_orig import (  # noqa: F401
+    DANDI_INSTANCE_URL_PATTERN,
+    DANDI_NSKEY,
+    get_schema_version,
+)
 
 # TODO: temporary imports of consts etc which might need to be 'redone'
 # so we do not duplicate them
